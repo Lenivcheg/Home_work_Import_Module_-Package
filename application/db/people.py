94ad3d277d9702fbefcb3a7f18a95ed393ee26ee
Sergey Nogin <noginn@ya.ru>
2024-01-23 22:10:34 +0300

    first_commit

---
+++ application/db/people.py
@@ -0,0 +1,8 @@
+def get_employees():
+    print('Функция get_employees')
+
+# Конструкция чтобы код не запускался при импорте этого модуля.
+
+
+if __name__ == '__main__':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,8 @@
+def calculate_salary():
+    print('Функция calculate_salary')
+
+# Конструкция чтобы код не запускался при импорте этого модуля.
+
+
+if __name__ == '__main__':
+    calculate_salary()
